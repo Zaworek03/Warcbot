fe4bbbbd002af41e5cb1d754a4ddff5dfecb6714
Bartosz Zawlocki <zawlockibartosz@icloud.com>
2026-07-17 14:36:35 +0200

    Naprawa merge

--- Logical/Libraries/MpAlarmX/SG4/MpAlarmX.h
+++ Logical/Libraries/MpAlarmX/SG4/MpAlarmX.h
@@ -1,6 +1,6 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
-/* MpAlarmX 6.7.1 */
+/* MpAlarmX 6.7.0 */
 
 #ifndef _MPALARMX_
 #define _MPALARMX_
@@ -9,7 +9,7 @@ extern "C"
 {
 #endif
 #ifndef _MpAlarmX_VERSION
-#define _MpAlarmX_VERSION 6.7.1
+#define _MpAlarmX_VERSION 6.7.0
 #endif
 
 #include <bur/plctypes.h>
